5c6a7185703b0bc676fcc13634b28a3e01f95f56
Martin Homuth <mh@emlix.com>
2020-09-24 19:29:07 +0200

    various updates
    Signed-off-by: Martin Homuth <mh@emlix.com>

--- init.el
+++ init.el
@@ -2,14 +2,12 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-(package-initialize nil)
-
 (add-to-list 'load-path "~/elisp/org-mode/lisp")
 (add-to-list 'load-path "~/elisp/org-mode/contrib/lisp")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp")
 
-(package-initialize t)
 (setq package-enable-at-startup nil)
 (require 'org)
 (org-babel-load-file (concat (getenv "HOME") "/.emacs.d/" user-login-name ".org"))
 (put 'narrow-to-region 'disabled nil)
+(put 'list-timers 'disabled nil)
