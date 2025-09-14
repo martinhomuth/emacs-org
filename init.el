7b1dbfc107eff1f48c7a609d156f1e45241c8d98
Martin Homuth <martin@followthestack.tech>
2025-09-14 06:56:16 +0200

    fix: frame-alist: remove unused value
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- init.el
+++ init.el
@@ -21,9 +21,7 @@
 ;; TODO: add default font and fontsize
 (defvar mh-fontsize 12)
 (add-to-list 'default-frame-alist
-             `(font . ,(concat "Iosevka-" (number-to-string mh-fontsize)))
-             `(font . ,(concat "Noto Color Emoji" (number-to-string mh-fontsize)))
-             )
+             `(font . ,(concat "Iosevka-" (number-to-string mh-fontsize))))
 
 ;; Treat clipboard as UTF-8 string first
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
