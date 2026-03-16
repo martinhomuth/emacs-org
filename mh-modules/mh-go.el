313f2736794262e1299de4c653094eec98b2851e
Martin Homuth <martin@followthestack.tech>
2026-03-16 05:12:11 +0100

    feat: add go-mode and configuration
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- mh-modules/mh-go.el
+++ mh-modules/mh-go.el
@@ -10,18 +10,20 @@
   (cdr project))
 
 (add-hook 'project-find-functions #'project-find-go-module)
-(require 'go-mode)
-(require 'eglot)
-(add-hook 'go-mode-hook 'eglot-ensure)
+(use-package go-mode
+  :ensure t
+  :config
+  (require 'eglot)
+  (add-hook 'go-mode-hook 'eglot-ensure)
 
-;; Optional: install eglot-format-buffer as a save hook.
-;; The depth of -10 places this before eglot's willSave notification,
-;; so that notification reports the actual contents that will be saved.
-(defun eglot-format-buffer-before-save ()
-  (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
-(add-hook 'go-mode-hook #'eglot-format-buffer-before-save)
+  ;; Optional: install eglot-format-buffer as a save hook.
+  ;; The depth of -10 places this before eglot's willSave notification,
+  ;; so that notification reports the actual contents that will be saved.
+  (defun eglot-format-buffer-before-save ()
+    (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
+  (add-hook 'go-mode-hook #'eglot-format-buffer-before-save)
 
-(add-hook 'before-save-hook
-	  (lambda ()
-            (call-interactively 'eglot-code-action-organize-imports))
-	  nil t)
+  (add-hook 'before-save-hook
+	    (lambda ()
+              (call-interactively 'eglot-code-action-organize-imports))
+	    nil t))
